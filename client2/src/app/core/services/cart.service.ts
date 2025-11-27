f5e67eb014e914b50d8cfc1c105a64982dd3f0a3
Mahmoud Elsayed <pro.mahmoud.elsayed@gmail.com>
2025-11-27 16:44:15 +0300

    Add Singal and Cart Service

---
+++ client2/src/app/core/services/cart.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Cart } from '../../shared/models/cart';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  baseURL = environment.apiUrl;
+  private http = inject(HttpClient);
+  cart = signal<Cart | null>(null);
+
+  getCart(id: string) {
+    return this.http.get<Cart>(this.baseURL + 'cart?id=' + id).subscribe({
+      next: cart => this.cart.set(cart)
+    })
+  }
+
+  setCart(cart: Cart) {
+    return this.http.post<Cart>(this.baseURL + 'cart', cart).subscribe({
+      next: cart => this.cart.set(cart)
+    })
+  }
+  deleteCart(id:string){
+    return this.http.delete(this.baseURL + 'cart?id=' + id).subscribe({
+      next: () => this.cart.set(null)
+    })
+  }
+}
