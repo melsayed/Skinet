c976cd087906e4516f501f7cdb933e7cd9de4505
meslayed1987 <pro.mahmoud.elsayed@gmail.com>
2025-12-27 15:51:02 +0300

    To Video 164

--- client2/src/app/features/checkout/checkout.component.ts
+++ client2/src/app/features/checkout/checkout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
+import {MatStepper,MatStep} from '@angular/material/stepper';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [OrderSummaryComponent, MatStepper, MatStep],
   standalone : true,
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss',
